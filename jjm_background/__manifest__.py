6687fbec55f825d7695ca8e143ce625ee4e16c9f
Gabriela Rivero <regaby@gmail.com>
2021-07-26 19:02:24 -0300

    ADD background module

---
+++ jjm_background/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'JJM Background',
+    'version': '0.1',
+    'author': 'Ing. Gabriela Rivero',
+    'license': 'LGPL-3',
+    'website': 'www.galup.com.ar',
+    'description': 'jjm web responsive background',
+    'depends': ['web_responsive'],
+    'data': [
+        "views/assets.xml",
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+}

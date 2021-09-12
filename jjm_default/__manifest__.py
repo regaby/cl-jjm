d692d1e9cd8af19c063cba0b32e04a9a2fad85bf
Gabriela Rivero <regaby@gmail.com>
2021-09-12 20:43:01 -0300

    ADD server-brand

--- jjm_default/__manifest__.py
+++ jjm_default/__manifest__.py
@@ -168,6 +168,7 @@
         'https://github.com/OCA/partner-contact.git',
         'https://github.com/OCA/server-tools.git',
         'https://github.com/OCA/knowledge.git',
+        'https://github.com/OCA/server-brand.git',
         ##
         # 'https://github.com/regaby/contract.git',
         #'https://github.com/ctmil/contract.git ctmil/contract',
